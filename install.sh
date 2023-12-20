b4099088001bac404595ea61a9d3a04fc164e4aa
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2023-12-20 12:23:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,6 +33,6 @@ cd usr/lib64
 sudo cp -r * /opt/resolve/libs/
 
 # installing h.264 video encoder plugin (export only)
-sudo cp -r ~/fedora-resolve/h264/* /opt/resolve/IOPlugins/
+cp -r ~/fedora-resolve/h264/* /opt/resolve/IOPlugins/
 
 notify-send "installation completed!"
