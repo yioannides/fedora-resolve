9edb7e94d8413a4f3b53e35c05235aa3f2ac7d31
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2024-08-18 22:18:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo
 sleep 1
 
 # installing prerequisites
-dnf install -y libxcrypt-compat libcurl libcurl-devel mesa-libGLU
+sudo dnf install -y libxcrypt-compat libcurl libcurl-devel mesa-libGLU
 
 # installing NVIDIA drivers
 lspci | grep -qi "NVIDIA" && sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
@@ -21,8 +21,8 @@ SKIP_PACKAGE_CHECK=1 ./*.run -i -y
 # resolving dependencies
 cd /opt/resolve/libs
 mkdir disabled-libraries
-mv libglib* libgio* libmodule* libgobject* disabled-libraries
-# cp -f /usr/lib64/libglib-2.0.so.0.* /opt/resolve/libs/
+sudo mv libglib* libgio* libmodule* libgobject* disabled-libraries
+# sudo cp -f /usr/lib64/libglib-2.0.so.0.* /opt/resolve/libs/
 
 # installing h.264 video encoder plugin (export only)
 cp -r '/home/$USER/fedora-resolve/h264/'* /opt/resolve/IOPlugins/
