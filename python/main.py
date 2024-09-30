cc8aed9726c59b540ece3750bb85342c39cfc267
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2024-09-30 17:10:31 +0300

    Added Python scripts

---
+++ python/main.py
@@ -0,0 +1,18 @@
+import sys
+from resolve_download import *
+
+if __name__ == "__main__":
+    is_studio = '--studio' in sys.argv
+    is_beta = '--beta' in sys.argv
+    app_tag = "davinci-resolve-studio" if is_studio else "davinci-resolve"
+
+    print("Building for tag " + app_tag)
+
+    print("Requesting version information...")
+    (version, release_id, download_id) = get_latest_version_information(
+        refer_id='77ef91f67a9e411bbbe299e595b4cfcc',
+        app_tag=app_tag,
+        stable=is_beta is False,
+    )
+    print(f"Download latest version of Davinci Resolve{' Studio' if is_studio else ''}...")
+    download_using_id(download_id)
