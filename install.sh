805f64434cdd751d619402f89555fccc817831d9
yiannis ioannides <154235768+yioannides@users.noreply.github.com>
2024-09-30 16:19:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,4 +34,4 @@ echo
 echo "Installation completed!"
 
 # to edit
-mkdir -p transcoded; for i in *.mp4; do ffmpeg -i "$i" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "transcoded/${i%.*}.mov"; done
+'alias transcode="mkdir -p transcoded; for i in *.mp4; do ffmpeg -i "$i" -vcodec mjpeg -q:v 2 -acodec pcm_s16be -q:a 0 -f mov "transcoded/${i%.*}.mov"; done"' | tee -a /home/$USER/.bashrc
